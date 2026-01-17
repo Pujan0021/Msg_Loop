5a72e64fb0e3f9f0014c847776d443cd9db0b9ad
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-17 21:40:31 +0545

    Auth Route

--- Server/server.js
+++ Server/server.js
@@ -3,14 +3,14 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
+const signupRouter = require("./src/routes/auth.route")
+app.use("/api", signupRouter)
 app.get("/", (req, res) => {
     res.send({
         message: "Hello From Server"
     })
 })
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is Running at Port: ${PORT}`)
