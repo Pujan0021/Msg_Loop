178671e15bd03bcb1bcba70999a53ba70014cbec
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-18 22:47:12 +0545

    Login Route

--- Server/src/routes/auth.route.js
+++ Server/src/routes/auth.route.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 router.post("/auth/signup", async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -38,11 +41,57 @@ router.post("/auth/signup", async (req, res) => {
         })
     } catch (error) {
         console.log("Signup Failed !", error.message);
-        res.status(200).send({
+        res.status(500).send({
             success: false,
             message: "Signup Failed!"
         })
     }
 }
-)
+);
+router.post("/auth/login", async (req, res) => {
+    const JWT_SECRET = process.env.JWT_SECRET;
+    // console.log(JWT_SECRET)
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).send({
+            success: false,
+            message: "All the fields are required!"
+        })
+    }
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "User does not exist!"
+            })
+        }
+        const verifyPassword = await bcrypt.compare(password, user.password);
+        if (!verifyPassword) {
+            return res.status(401).send({
+                success: false,
+                message: "Invalid email or password!"
+            })
+        }
+        const token = jwt.sign({ id: user._id }, JWT_SECRET, {
+            expiresIn: "1h"
+        });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 60 * 60 * 1000 //1h
+        });
+        res.status(200).send({
+            success: true,
+            message: "Login Successfully"
+        })
+    } catch (error) {
+        console.log("Login Failed !", error.message);
+        res.status(500).send({
+            success: false,
+            message: "Login Failed!"
+        })
+    }
+})
 module.exports = router;
