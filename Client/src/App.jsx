a090f7e47860173b23a29b5d972b6438d259ba6f
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-19 20:04:59 +0545

    Initial Commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Signup from "../src/pages/Signup";
+import Login from "../src/pages/Login";
+
+const App = () => {
+  return (
+    <>
+      <Login></Login>
+      {/* <Signup></Signup> */}
+    </>
+  );
+};
+
+export default App;
