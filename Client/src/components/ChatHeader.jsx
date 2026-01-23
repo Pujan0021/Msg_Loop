5ed56ccf4ee267d5cec3a503fc00f32e6a6ff00e
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-23 23:23:08 +0545

    Refactored ChatHeader

--- Client/src/components/ChatHeader.jsx
+++ Client/src/components/ChatHeader.jsx
@@ -5,11 +5,15 @@ import { FiLogOut } from "react-icons/fi";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Contacts from "./Contacts";
+import ChatPage from "./ChatPage";
 
 const ChatHeader = () => {
+  const { activeTab } = useChatStore();
   const navigate = useNavigate();
   const { userDetail } = useContext(UserAuthContext);
-  const { activeTab } = useChatStore();
+  const { handleChats, handleContacts } = useChatStore();
+
   const handleLogOut = async () => {
     try {
       const res = await axios.post(
@@ -32,11 +36,11 @@ const ChatHeader = () => {
 
   return (
     <>
-      <div className="flex font-orbitron justify-center items-center h-screen flex-col">
+      <div className="flex font-orbitron justify-center items-center h-screen     flex-col">
         <div className=" text-center py-2 px-2  text-xl font-bold bg-gray-800 text-white rounded-sm w-90">
           Msg_Loop
         </div>
-        <div className="shadow-2xl shadow-blue-950 p-5 rounded-sm  bg-gray-800 text-white mt-1">
+        <div className="shadow-2xl shadow-blue-950 p-3 rounded-sm h-72 bg-gray-800 text-white mt-1 overflow-y-scroll  overflow-x-hidden hide-scrollbar">
           <div className="flex justify-between items-center">
             <div className="flex justify-start px-3 flex-col">
               <div className=" h-10 rounded-full  px-5">
@@ -56,14 +60,21 @@ const ChatHeader = () => {
           </div>
 
           <div className="flex border-t-2 border-white justify-center gap-15 w-80 mt-5">
-            <div className="bg-cyan-600 px-5 rounded-sm text-white py-1 mt-2 font-semibold text-sm cursor-pointer">
+            <div
+              onClick={handleChats}
+              className="bg-cyan-600 px-5 rounded-sm text-white py-1 mt-2 font-semibold text-sm cursor-pointer"
+            >
               Chats
             </div>
 
-            <div className="bg-cyan-600 px-5 rounded-sm text-white py-1 mt-2 font-semibold text-sm cursor-pointer">
+            <div
+              onClick={handleContacts}
+              className="bg-cyan-600 px-5 rounded-sm text-white py-1 mt-2 font-semibold text-sm cursor-pointer"
+            >
               Contacts
             </div>
           </div>
+          {activeTab == "contacts" ? <Contacts /> : <ChatPage />}
         </div>
       </div>
     </>
