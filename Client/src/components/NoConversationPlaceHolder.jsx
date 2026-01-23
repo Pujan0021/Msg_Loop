b9a0fbb14bd4f1690dcf0df47d003f73cbe78f7a
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-23 23:22:05 +0545

    NoConversationPlaceholder

---
+++ Client/src/components/NoConversationPlaceHolder.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoConversationPlaceHolder = () => {
+  return (
+    <>
+      <div className="shadow-2xl flex font-orbitron justify-center items-center mt-12  h-72  flex-col shadow-blue-950 p-5 rounded-sm bg-gray-800  text-white w-80 ">
+        <div className="px-3  text-[14px] py-3">Select User to text.</div>
+      </div>
+    </>
+  );
+};
+
+export default NoConversationPlaceHolder;
