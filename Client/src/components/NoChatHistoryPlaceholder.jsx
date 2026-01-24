1b762ef47205704e67e74e1764e7690a24a71b20
Pujan-Pandey <pujanpandey21@gmail.com>
2026-01-24 23:01:27 +0545

    NoChatHistoryPlaceHolder

---
+++ Client/src/components/NoChatHistoryPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoChatHistoryPlaceholder = () => {
+  return (
+    <div className="text-white hide-scrollbar font-orbitron flex justify-center items-center h-35">
+      No Chat History
+    </div>
+  );
+};
+
+export default NoChatHistoryPlaceholder;
